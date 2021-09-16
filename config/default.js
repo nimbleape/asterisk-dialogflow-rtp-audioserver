1db57d1932dae33ac67ab1f10091a6e104a6553f
Walter Rodriguez <wrodriguez1983@outlook.com>
2021-09-16 20:03:26 -0300

    Clean code

--- config/default.js
+++ config/default.js
@@ -1,47 +1,47 @@
 module.exports = {
   rtpServer: {
-    port: 7777,
-    host: '0.0.0.0',
-    swap16: true,
+      port: 7777,
+      host: '0.0.0.0',
+      swap16: true
   },
   mqtt: {
-    url: 'mqtt://test.mosquitto.org',
-    prefix: 'dialogflow-asterisk',
+      url: 'mqtt://test.mosquitto.org',
+      prefix: 'dialogflow-asterisk'
   },
   dialogflow: {
-    apiEndpoint: 'api-endpoint',
-    auth: {
-      //credentials: {
-      //     private_key: response.dialogFlowPrivateKey,
-      //     client_email: response.dialogFlowClientEmail
-      //   }
-      keyFilename: `./path-to-json.json`,
-    },
-    agentiId: 'agent-id',
-    project: 'project-id',
-    location: 'location',
-    initialEventName: 'WELCOME',
-    enableOutputSpeech: true,
-    audioInputConfig: {
-      audioEncoding: 'AUDIO_ENCODING_LINEAR_16',
-      sampleRateHertz: 16000,
-      languageCode: 'en',
-      singleUtterance: false,
-    },
-    audioOutputConfig: {
-      audioEncoding: 'OUTPUT_AUDIO_ENCODING_LINEAR_16',
-      sampleRateHertz: 8000, // should be the same as dialogFlowAudioInputConfig.sampleRateHertz but Asterisk doesnt like 16000 back
-      /*
-        comment the below object if you dont want to change any aspects of the generated voice
-      */
-      synthesizeSpeechConfig: {
-        speakingRate: 1,
-        pitch: 5,
-        volumeGainDb: 0,
-        voice: {
-          ssmlGender: `SSML_VOICE_GENDER_FEMALE`,
-        },
+      apiEndpoint: 'api-endpoint',
+      auth: {
+          //credentials: {
+          //     private_key: response.dialogFlowPrivateKey,
+          //     client_email: response.dialogFlowClientEmail
+          //   }
+          keyFilename: `./path-to-json.json`,
       },
-    },
-  },
-};
+      agentId: 'agent-id',
+      project: 'project-id',
+      location: 'location',
+      initialEventName: 'WELCOME',
+      enableOutputSpeech: true,
+      audioInputConfig: {
+          audioEncoding: 'AUDIO_ENCODING_LINEAR_16',
+          sampleRateHertz: 16000,
+          languageCode: 'en',
+          singleUtterance: false
+      },
+      audioOutputConfig: {
+          audioEncoding: 'OUTPUT_AUDIO_ENCODING_LINEAR_16',
+          sampleRateHertz: 8000, // should be the same as dialogFlowAudioInputConfig.sampleRateHertz but Asterisk doesnt like 16000 back
+          /*
+            comment the below object if you dont want to change any aspects of the generated voice
+           */
+          synthesizeSpeechConfig: {
+              speakingRate: 1,
+              pitch: 5,
+              volumeGainDb: 0,
+              voice: {
+                  ssmlGender: `SSML_VOICE_GENDER_FEMALE`
+              }
+          }
+      }
+  }
+}
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mqtt = require('async-mqtt');
 const Pino = require('pino');
 const DialogFlowConnector = require('./lib/DialogFlowConnector');
 const log = new Pino({
-  name: 'Dialogflow-AudioServer',
+    name: 'Dialogflow-AudioServer',
 });
 
 let rtpServer = new RtpServer(config.get('rtpServer'), log);
@@ -14,99 +14,86 @@ let connectorsMap = new Map();
 let mqttClient;
 
 async function createNewGoogleStream(payload) {
-  log.info(
-    { payload },
-    'New Stream of audio from Asterisk to send to Dialogflow'
-  );
 
-  const dialogFlowConfig = {
-    auth: config.get('dialogflow.auth'),
-    projectId: config.get('dialogflow.project'),
-    location: config.get('dialogflow.location'),
-    agentId: config.get('dialogflow.agentId'),
-    apiEndpoint: config.get('dialogflow.apiEndpoint'),
-    sessionId: payload.channelId,
-    initialEventName: config.get('dialogflow.initialEventName'),
-    enableOutputSpeech: config.get('dialogflow.enableOutputSpeech'),
-  };
+    log.info({ payload }, 'New Stream of audio from Asterisk to send to Dialogflow');
 
-  let dialogflowConnector = new DialogFlowConnector(
-    {
-      input: config.get('dialogflow.audioInputConfig'),
-      output: config.get('dialogflow.audioOutputConfig'),
-    },
-    dialogFlowConfig,
-    payload.channelId,
-    log
-  );
+    const dialogFlowConfig = {
+        auth: config.get('dialogflow.auth'),
+        projectId: config.get('dialogflow.project'),
+        location: config.get('dialogflow.location'),
+        agentId: config.get('dialogflow.agentId'),
+        apiEndpoint: config.get('dialogflow.apiEndpoint'),
+        sessionId: payload.channelId,
+        initialEventName: config.get('dialogflow.initialEventName'),
+        enableOutputSpeech: config.get('dialogflow.enableOutputSpeech')
+    };
 
-  let audioDataStream = rtpServer.createStream(payload.port);
+    let dialogflowConnector = new DialogFlowConnector({
+        input: config.get('dialogflow.audioInputConfig'),
+        output: config.get('dialogflow.audioOutputConfig')
+    }, dialogFlowConfig, payload.channelId, log);
 
-  dialogflowConnector.start(audioDataStream);
+    let audioDataStream = rtpServer.createStream(payload.port);
 
-  connectorsMap.set(payload.channelId, dialogflowConnector);
+    dialogflowConnector.start(audioDataStream);
 
-  dialogflowConnector.on('message', async (data) => {
-    log.info(
-      `Got a message sending to ${mqttTopicPrefix}/${payload.channelId}/events`
-    );
-    await mqttClient.publish(
-      `${mqttTopicPrefix}/${payload.channelId}/events`,
-      JSON.stringify(data)
-    );
-  });
+    connectorsMap.set(payload.channelId, dialogflowConnector);
+
+    dialogflowConnector.on('message', async (data) => {
+        log.info(`Got a message sending to ${mqttTopicPrefix}/${payload.channelId}/events`);
+        await mqttClient.publish(`${mqttTopicPrefix}/${payload.channelId}/events`, JSON.stringify(data));
+    });
 }
 
 function stopDialogflowStream(payload) {
-  log.info(
-    { payload },
-    'Ending stream of audio from Asterisk to send to Dialogflow'
-  );
+    log.info({ payload }, 'Ending stream of audio from Asterisk to send to Dialogflow');
 
-  let connector = connectorsMap.get(payload.channelId);
+    let connector = connectorsMap.get(payload.channelId);
 
-  if (connector) {
-    connector.close();
-    connectorsMap.delete(payload.channelId);
-  }
+    if (connector) {
+        connector.close();
+        connectorsMap.delete(payload.channelId);
+    }
 
-  rtpServer.endStream(payload.port);
+    rtpServer.endStream(payload.port);
 }
 
 async function run() {
-  mqttClient = await mqtt.connectAsync(config.get('mqtt.url'));
-  log.info('Connected to MQTT');
 
-  await mqttClient.subscribe(`${mqttTopicPrefix}/newStream`);
-  await mqttClient.subscribe(`${mqttTopicPrefix}/streamEnded`);
-  log.info('Subscribed to both newStream & streamEnded topic');
+    mqttClient = await mqtt.connectAsync(config.get('mqtt.url'));
+    log.info('Connected to MQTT');
 
-  mqttClient.on('message', (topic, message) => {
-    let payload = JSON.parse(message.toString());
+    await mqttClient.subscribe(`${mqttTopicPrefix}/newStream`);
+    await mqttClient.subscribe(`${mqttTopicPrefix}/streamEnded`);
+    log.info('Subscribed to both newStream & streamEnded topic');
 
-    switch (topic) {
-      case `${mqttTopicPrefix}/newStream`:
-        createNewGoogleStream(payload);
-        break;
-      case `${mqttTopicPrefix}/streamEnded`:
-        stopDialogflowStream(payload);
-        break;
-      default:
-        break;
-    }
-  });
+    mqttClient.on('message', (topic, message) => {
+        let payload = JSON.parse(message.toString());
 
-  rtpServer.on('err', (err) => {
-    streamsMap.forEach((stream, key) => {
-      stream.end();
-      streamsMap.delete(key);
+        switch(topic) {
+            case `${mqttTopicPrefix}/newStream`:
+                createNewGoogleStream(payload);
+                break;
+            case `${mqttTopicPrefix}/streamEnded`:
+                stopDialogflowStream(payload);
+                break;
+            default:
+                break;
+        }
     });
 
-    throw err;
-  });
+    rtpServer.on('err', (err) => {
 
-  rtpServer.bind();
-  log.info('AudioServer listening on UDP port');
+        streamsMap.forEach((stream, key) => {
+            stream.end();
+            streamsMap.delete(key);
+        });
+
+        throw err;
+    });
+
+    rtpServer.bind();
+    log.info('AudioServer listening on UDP port');
 }
 
 run();
