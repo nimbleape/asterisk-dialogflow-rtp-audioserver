68b14619028d6fe7afa79b6555ff8abfbb5cc018
Walter Rodriguez <wrodriguez1983@outlook.com>
2021-09-16 19:19:35 -0300

    Update to Dialogflow CX

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mqtt = require('async-mqtt');
 const Pino = require('pino');
 const DialogFlowConnector = require('./lib/DialogFlowConnector');
 const log = new Pino({
-    name: 'Dialogflow-AudioServer',
+  name: 'Dialogflow-AudioServer',
 });
 
 let rtpServer = new RtpServer(config.get('rtpServer'), log);
@@ -14,83 +14,99 @@ let connectorsMap = new Map();
 let mqttClient;
 
 async function createNewGoogleStream(payload) {
+  log.info(
+    { payload },
+    'New Stream of audio from Asterisk to send to Dialogflow'
+  );
 
-    log.info({ payload }, 'New Stream of audio from Asterisk to send to Dialogflow');
+  const dialogFlowConfig = {
+    auth: config.get('dialogflow.auth'),
+    projectId: config.get('dialogflow.project'),
+    location: config.get('dialogflow.location'),
+    agentId: config.get('dialogflow.agentId'),
+    apiEndpoint: config.get('dialogflow.apiEndpoint'),
+    sessionId: payload.channelId,
+    initialEventName: config.get('dialogflow.initialEventName'),
+    enableOutputSpeech: config.get('dialogflow.enableOutputSpeech'),
+  };
 
-    const dialogFlowConfig = {
-        auth: config.get('dialogflow.auth'),
-        projectId: config.get('dialogflow.project'),
-        sessionId: payload.channelId,
-        initialEventName: config.get('dialogflow.initialEventName'),
-        enableOutputSpeech: config.get('dialogflow.enableOutputSpeech')
-    };
+  let dialogflowConnector = new DialogFlowConnector(
+    {
+      input: config.get('dialogflow.audioInputConfig'),
+      output: config.get('dialogflow.audioOutputConfig'),
+    },
+    dialogFlowConfig,
+    payload.channelId,
+    log
+  );
 
-    let dialogflowConnector = new DialogFlowConnector({
-        input: config.get('dialogflow.audioInputConfig'),
-        output: config.get('dialogflow.audioOutputConfig')
-    }, dialogFlowConfig, payload.channelId, log);
+  let audioDataStream = rtpServer.createStream(payload.port);
 
-    let audioDataStream = rtpServer.createStream(payload.port);
+  dialogflowConnector.start(audioDataStream);
 
-    dialogflowConnector.start(audioDataStream);
+  connectorsMap.set(payload.channelId, dialogflowConnector);
 
-    connectorsMap.set(payload.channelId, dialogflowConnector);
-
-    dialogflowConnector.on('message', async (data) => {
-        log.info(`Got a message sending to ${mqttTopicPrefix}/${payload.channelId}/events`);
-        await mqttClient.publish(`${mqttTopicPrefix}/${payload.channelId}/events`, JSON.stringify(data));
-    });
+  dialogflowConnector.on('message', async (data) => {
+    log.info(
+      `Got a message sending to ${mqttTopicPrefix}/${payload.channelId}/events`
+    );
+    await mqttClient.publish(
+      `${mqttTopicPrefix}/${payload.channelId}/events`,
+      JSON.stringify(data)
+    );
+  });
 }
 
 function stopDialogflowStream(payload) {
-    log.info({ payload }, 'Ending stream of audio from Asterisk to send to Dialogflow');
+  log.info(
+    { payload },
+    'Ending stream of audio from Asterisk to send to Dialogflow'
+  );
 
-    let connector = connectorsMap.get(payload.channelId);
+  let connector = connectorsMap.get(payload.channelId);
 
-    if (connector) {
-        connector.close();
-        connectorsMap.delete(payload.channelId);
-    }
+  if (connector) {
+    connector.close();
+    connectorsMap.delete(payload.channelId);
+  }
 
-    rtpServer.endStream(payload.port);
+  rtpServer.endStream(payload.port);
 }
 
 async function run() {
+  mqttClient = await mqtt.connectAsync(config.get('mqtt.url'));
+  log.info('Connected to MQTT');
 
-    mqttClient = await mqtt.connectAsync(config.get('mqtt.url'));
-    log.info('Connected to MQTT');
+  await mqttClient.subscribe(`${mqttTopicPrefix}/newStream`);
+  await mqttClient.subscribe(`${mqttTopicPrefix}/streamEnded`);
+  log.info('Subscribed to both newStream & streamEnded topic');
 
-    await mqttClient.subscribe(`${mqttTopicPrefix}/newStream`);
-    await mqttClient.subscribe(`${mqttTopicPrefix}/streamEnded`);
-    log.info('Subscribed to both newStream & streamEnded topic');
+  mqttClient.on('message', (topic, message) => {
+    let payload = JSON.parse(message.toString());
 
-    mqttClient.on('message', (topic, message) => {
-        let payload = JSON.parse(message.toString());
+    switch (topic) {
+      case `${mqttTopicPrefix}/newStream`:
+        createNewGoogleStream(payload);
+        break;
+      case `${mqttTopicPrefix}/streamEnded`:
+        stopDialogflowStream(payload);
+        break;
+      default:
+        break;
+    }
+  });
 
-        switch(topic) {
-            case `${mqttTopicPrefix}/newStream`:
-                createNewGoogleStream(payload);
-                break;
-            case `${mqttTopicPrefix}/streamEnded`:
-                stopDialogflowStream(payload);
-                break;
-            default:
-                break;
-        }
+  rtpServer.on('err', (err) => {
+    streamsMap.forEach((stream, key) => {
+      stream.end();
+      streamsMap.delete(key);
     });
 
-    rtpServer.on('err', (err) => {
+    throw err;
+  });
 
-        streamsMap.forEach((stream, key) => {
-            stream.end();
-            streamsMap.delete(key);
-        });
-
-        throw err;
-    });
-
-    rtpServer.bind();
-    log.info('AudioServer listening on UDP port');
+  rtpServer.bind();
+  log.info('AudioServer listening on UDP port');
 }
 
 run();
